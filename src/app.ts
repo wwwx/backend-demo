8f255fd229847f5c1eb139ffea007e29ba7106b2
Wang Xu <wangxu1@jiayinfintech.cn>
2022-01-27 15:28:19 +0800

    chore(*): setup eslint

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import * as http from 'http';
-import 'dotenv/config';
+import * as http from 'http'
+import 'dotenv/config'
 
 if (!process.env.PORT) {
-  process.exit(1);
+  process.exit(1)
 }
 
-const PORT = parseInt(process.env.PORT, 10);
+const PORT = parseInt(process.env.PORT, 10)
 
-const server = http.createServer((_req, res) => {
-  res.end('OK');
-});
+const server = http.createServer((req, res) => {
+  res.end('OK')
+})
 
 server.listen(PORT, () => {
-  console.log('server listening at http://localhost:' + PORT);
-});
+  console.log('server listening at http://localhost:' + PORT)
+})
