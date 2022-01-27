eb40d41093a5ec8a44b6e877bcbc2a078727fec5
Wang Xu <wangxu1@jiayinfintech.cn>
2022-01-27 11:28:13 +0800

    chore: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import * as http from "http";
-const PORT = 3000;
+import * as http from 'http';
+import 'dotenv/config';
 
-const server = http.createServer((req, res) => {
-  res.end("OK");
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT = parseInt(process.env.PORT, 10);
+
+const server = http.createServer((_req, res) => {
+  res.end('OK');
 });
 
 server.listen(PORT, () => {
-  console.log("http://localhost:" + PORT);
+  console.log('server listening at http://localhost:' + PORT);
 });
